052d0c2dab25ff9cc475f22446a1946094e9e6b3
Ilya Shusterman <ilyashusterman@outlook.com>
2017-02-23 10:43:33 +0200

    login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import {User} from './User';
 import axios from 'axios';
 import { Http, Headers, Response } from '@angular/http';
 import {forEach} from "@angular/router/src/utils/collection";
+import {Observable} from "rxjs";
 
 
 @Component({
@@ -17,7 +18,7 @@ export class LoginComponent implements OnInit {
   constructor(public authService: AuthService, public router: Router) { }
 
   isChecked:boolean = false;
-  errorMessage:string ='Login Failed';
+  errorMessage:string ='Login Failed ';
   loginApproved: boolean = false;
   loading: boolean = false;
   debugWindow: boolean = false;
@@ -46,11 +47,14 @@ export class LoginComponent implements OnInit {
           this.router.navigate([redirect]);
       })
       .catch(function (error) {
-       let message = JSON.stringify(error.message);
-      //  console.log(message);
-         this.setErrorMessage(message);
+       let message = { errorMessage: error.response.data };
+         console.log(message.errorMessage);
+        let msg = message.errorMessage +' ' ;
+         this.setErrorMessage(msg);
+        //this.handleError(error);
       });
   }
+
 
 
   setLogin(){
@@ -62,10 +66,16 @@ export class LoginComponent implements OnInit {
   setChecked(){
     this.isChecked= true;
   }
-  setErrorMessage(message: String){
-   console.log(message);
+  setErrorMessage(message :string){
+    this.errorMessage=message;
   }
-
+  private handleError(error: Response) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    console.error(error);
+    this.setErrorMessage('Authentucation error '+error.statusText)
+    return Observable.throw(error.json().error || 'Server error');
+  }
   ngOnInit(): void {
     // let newUser = localStorage.getItem("user");
     // let newIsLoggedIn = localStorage.getItem("isLoggedIn");
