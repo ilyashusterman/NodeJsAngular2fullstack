74b15ba29fa43d67532d027e3c1db32ede4bb00b
Ilya Shusterman <ilyashusterman@outlook.com>
2017-02-23 12:44:55 +0200

    session validation with hashed cypte token

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
   public login(): boolean {
   //  Observable.of(true).delay(1000).do(val => this.isLoggedIn = true);
     let logged = localStorage.getItem("token");
-    console.log("login logged="+logged);
+    //console.log("login logged="+logged);
     return this.checkTokenSession(logged);
 
   }
@@ -36,7 +36,7 @@ export class AuthService {
       token: logged
     })
       .then(function (response) {
-        console.log('response is good '+response.data);
+        //console.log('response is good '+response.data);
         valid = true;
       })
       .catch(function (error) {
