6a6823b820fe17e25ddec3ee957290c7f7381d35
ildevelop <raduilya@gmail.com>
2017-03-07 11:22:56 +0200

    User checkbox  service checked

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -7,6 +7,10 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/trow';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import { Observable } from 'rxjs/Observable';
 import {User} from "./login/User";
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -6,6 +9,8 @@ import { LoginComponent } from './login.component';
 @NgModule({
   imports: [
     CommonModule,
+    BrowserModule,
+    FormsModule,
     LoginRoutingModule
   ],
   declarations: [LoginComponent]
